4dbcd1d062b9fb62a3df3c4b84d12f9f5d1ab56a
Daniel Oliveira <danielolivep@gmail.com>
2021-06-19 15:36:31 -0300

    Create first collection type - Categories

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f773bffbbc511ad0c919ca919f78747d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3ee819b4-0228-4492-9c73-1583eaad2ddb'
+};
