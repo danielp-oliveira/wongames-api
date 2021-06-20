9bd12d5e2754498d56163c9adfb2461c12968cea
Daniel Oliveira <danielolivep@gmail.com>
2021-06-20 10:48:22 -0300

    Change LeftMenuFooter

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Wrapper, { A } from "./Wrapper";
+
+function LeftMenuFooter() {
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        Powered by &nbsp;
+        <A
+          key="website"
+          href="https://reactavancado.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Avançado
+        </A>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
