b249854fcf0642ea78f63cdc8124d799088e1e5d
Daniel Oliveira <danielolivep@gmail.com>
2021-06-22 21:34:36 -0300

    Add create games

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -75,6 +75,40 @@ async function createManyToManyData(products) {
   ])
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, 'game')
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`)
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, '-'),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, 'category'))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, 'platform')
+            )
+          ),
+          developers: [await getByName(product.developer, 'developer')],
+          publisher: await getByName(product.publisher, 'publisher'),
+          ...(await getGameInfo(product.slug))
+        })
+
+        return game
+      }
+    })
+  )
+}
+
 module.exports = {
   populate: async () => {
     const gogApiUrl =
@@ -85,6 +119,7 @@ module.exports = {
     } = await axios.get(gogApiUrl)
 
     await createManyToManyData([products[2], products[3]])
+    await createGames([products[2], products[3]])
 
     // console.log(await getGameInfo(products[1].slug))
   }
